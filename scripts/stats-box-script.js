e2e1a7e3605867b104a3de2de0ad6a844b21423a
kenstercc <kenstercc@yahoo.com>
2025-02-12 11:52:41 -0500

    Update stats-box-script.js

--- scripts/stats-box-script.js
+++ scripts/stats-box-script.js
@@ -1,5 +1,5 @@
 // Fetch data from external file (e.g., JSON)
-fetch('/data/stats-box-data.json')
+fetch('data/stats-box-data.json')
     .then(response => response.json())
     .then(data => {
         const container = document.getElementById('stats-data-container');
